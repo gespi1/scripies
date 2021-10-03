1f321e8010e419f7be1f7994d01930213bd48178
Giancarlo Espinoza <gespi1@users.noreply.github.com>
2021-10-03 10:51:28 -0400

    Update find_volume.sh

--- find_volume.sh
+++ find_volume.sh
@@ -3,7 +3,7 @@
 found=false
 ls -1 /tmp | while read line;
 do
-	if [ "$line" == "$(python rancher2.py --project ghwstore2 --namespace qa --claimspace magento2 --token token-mrbzj:b8qrhgk6bk4kt9ll9sn9j9xpw8s5ttw5mk4pfwzpbkw9wm5rdhlf2b)" ]
+	if [ "$line" == "$(python rancher2.py --project ghwstore2 --namespace qa --claimspace magento2 --token token-mrbzj: TOKEN)" ]
 	then
 		$found=true	
         fi
